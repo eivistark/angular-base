f15289bb6cc48658624c5c0ba0f043d0ab04ec2b
Josselyn Lorena Tuarez Cedeño <jtuarez@ec.krugercorp.com>
2021-09-23 16:09:44 -0500

    remove-keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: 'AIzaSyBGCVLoip1idTj_FGY9hgI-A52Zquv_h0M',
-    authDomain: 'safevote-c5f9f.firebaseapp.com',
-    databaseURL: 'https://safevote-c5f9f.firebaseio.com',
-    projectId: 'safevote-c5f9f',
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
     storageBucket: '',
-    messagingSenderId: '194830382218',
-    appId: '1:194830382218:web:e5e47fb220dcec9b'
+    messagingSenderId: '',
+    appId: ''
   }
 };
